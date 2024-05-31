466fe09503a5255c25d8fd1f60bd1caf2973d0ec
Amoo Oluwabukola <amoooluwabukola@outlook.com>
2024-05-31 13:45:30 +0100

    modifications

--- controllers/authController.js
+++ controllers/authController.js
@@ -126,7 +126,7 @@ const forgotPassword = async (req, res,next) => {
       }
       const resetToken = user.getResetPasswordToken();
       await user.save();
-      const resetUrl = `http://localhost:5173/ForgotPassword/${resetToken}`;
+      const resetUrl = `http://localhost:5173/resetpasswordlink/${resetToken}`;
       const message = `<h1>You have requested for a password reset </h1> <p>Please go to this link to reset your password</p> <a href=${resetUrl} clicktracking = off> ${resetUrl} </a> `;
       try {
         await sendEmail({
